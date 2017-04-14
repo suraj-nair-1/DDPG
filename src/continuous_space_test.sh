4e6be24ad9b5b1cd5ad60de6ba660f00dbe07ca7
surajnaircaltech <surajnair.caltech@gmail.com>
2017-04-13 17:54:59 -0700

    Prioritized Stopping condition

--- src/continuous_space_test.sh
+++ src/continuous_space_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./bin/HFO --frames-per-trial=500 --untouched-time=500 --offense-agents=1 --headless --fullstate --port 4200 --no-logging --seed 1234 &
+./bin/HFO --frames-per-trial=500 --untouched-time=500 --offense-agents=1 --headless --fullstate --port 4200 --no-logging --seed 4321 &
 sleep 5
 python ../DDPG/src/continuous_space_test_agent.py 6000 &
 #python ~/git/DDPG/tflearn/continuous_space_test_agent.py 6000 &
