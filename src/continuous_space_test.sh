b6a22c6a60459fd0ceef98ea76bdc5ac00f04265
surajnaircaltech <surajnair.caltech@gmail.com>
2017-04-16 23:06:51 -0700

    Multi agent

--- src/continuous_space_test.sh
+++ src/continuous_space_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-./bin/HFO --frames-per-trial=500 --untouched-time=500 --offense-agents=2 --defense-npcs=1 --headless --fullstate --port 6200 --no-logging --seed 6350 &
+./bin/HFO --frames-per-trial=500 --untouched-time=500 --offense-agents=2 --defense-npcs=1 --headless --fullstate --port 6300 --no-logging --seed 6360 &
 sleep 5
-python ../DDPG/src/continuous_space_test_agent.py 6200 1 6350 &
+python ../DDPG/src/continuous_space_test_agent.py 6300 1 6360 &
 sleep 5
-python ../DDPG/src/continuous_space_test_agent.py 6200 2 7350 &
+python ../DDPG/src/continuous_space_test_agent.py 6300 2 7360 &
 #python ~/git/DDPG/tflearn/continuous_space_test_agent.py 6000 &
 
 # sleep 5
