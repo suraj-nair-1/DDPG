2f862446988a28f114d3392340eecba165024bd5
surajnaircaltech <surajnair.caltech@gmail.com>
2017-04-30 11:32:45 -0700

    switch speedup

--- src/switchagent2.sh
+++ src/switchagent2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-./bin/HFO --headless --frames-per-trial=500 --untouched-time=500 --offense-agents 2 --defense-npcs 1 --fullstate --port 6000 --no-logging --seed 3212 &
+./bin/HFO --headless --frames-per-trial=500 --untouched-time=500 --offense-agents 2 --defense-npcs 1 --fullstate --port 6100 --no-logging --seed 3333 &
 sleep 5
-python ../DDPG/src/switchagent2.py 6000 1 5555 &
+python ../DDPG/src/switchagent2.py 6000 1 4333 &
 sleep 5
-python ../DDPG/src/switchagent2.py 6000 2 4444 &
+python ../DDPG/src/switchagent2.py 6000 2 3433 &
 #python ~/git/DDPG/tflearn/continuous_space_test_agent.py 6000 &
 
 # sleep 5
