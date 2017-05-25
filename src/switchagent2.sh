947ccb58a29c0639c4adb56a626e6f28fd68f66b
surajnaircaltech <surajnair.caltech@gmail.com>
2017-05-25 16:24:43 -0700

    bug fixing

--- src/switchagent2.sh
+++ src/switchagent2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-./bin/HFO --headless --frames-per-trial=500 --untouched-time=500 --offense-agents 2 --defense-npcs 1 --fullstate --port 5000 --no-logging --seed 212 &
+./bin/HFO --headless --frames-per-trial=500 --untouched-time=500 --offense-agents 2 --defense-npcs 1 --fullstate --port 5500 --no-logging --seed 121 &
 sleep 5
-python ../DDPG/src/switchagent2.py 5000 1 213 &
+python ../DDPG/src/switchagent2.py 5500 1 122 &
 sleep 5
-python ../DDPG/src/switchagent2.py 5000 2 214 &
+python ../DDPG/src/switchagent2.py 5500 2 123 &
 #python ~/git/DDPG/tflearn/continuous_space_test_agent.py 6000 &
 
 # sleep 5
