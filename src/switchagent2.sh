7e047ec2026dd515dabccce8cf8c66656908c838
surajnaircaltech <surajnair.caltech@gmail.com>
2017-06-03 19:51:09 -0700

    Updated reward func

--- src/switchagent2.sh
+++ src/switchagent2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-./bin/HFO --headless --frames-per-trial=500 --untouched-time=500 --offense-agents 2 --defense-npcs 1 --fullstate --port 2000 --no-logging --seed 144 &
+./bin/HFO --headless --frames-per-trial=500 --untouched-time=500 --offense-agents 2 --defense-npcs 1 --fullstate --port 4000 --no-logging --seed 988 &
 sleep 5
-python ../DDPG/src/switchagent2.py 2000 1 145 &
+python ../DDPG/src/switchagent2.py 4000 1 989 &
 sleep 5
-python ../DDPG/src/switchagent2.py 2000 2 146 &
+python ../DDPG/src/switchagent2.py 4000 2 990 &
 #python ~/git/DDPG/tflearn/continuous_space_test_agent.py 6000 &
 
 # sleep 5
