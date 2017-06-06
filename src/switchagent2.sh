5a89bed4e9d9209a626614086248bdc9a1baf333
surajnaircaltech <surajnair.caltech@gmail.com>
2017-06-05 17:11:35 -0700

    Debugging HFO

--- src/switchagent2.sh
+++ src/switchagent2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-./bin/HFO --headless --frames-per-trial=500 --untouched-time=500 --offense-agents 2 --defense-npcs 1 --fullstate --port 5500 --no-logging --seed 222 &
+./bin/HFO --headless --frames-per-trial=500 --untouched-time=500 --offense-agents 2 --defense-npcs 1 --fullstate --port 4000 --no-logging --seed 988 &
 sleep 5
-python ../DDPG/src/switchagent2.py 5500 1 223 &
+python ../DDPG/src/switchagent2.py 4000 1 989 &
 sleep 5
-python ../DDPG/src/switchagent2.py 5500 2 224 &
+python ../DDPG/src/switchagent2.py 4000 2 990 &
 #python ~/git/DDPG/tflearn/continuous_space_test_agent.py 6000 &
 
 # sleep 5
