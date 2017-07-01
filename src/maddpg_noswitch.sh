6ae5bed84567959c87d8beb6d53536230e036193
surajnaircaltech <surajnair.caltech@gmail.com>
2017-06-30 20:58:24 -0700

    MADDPG

--- src/maddpg_noswitch.sh
+++ src/maddpg_noswitch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-./bin/HFO --frames-per-trial=500 --untouched-time=500 --fullstate --headless --offense-agents=2 --defense-agents=1 --agent-play-goalie --no-logging --seed 200 --port 5000 &
+./bin/HFO --frames-per-trial=500 --untouched-time=500 --fullstate --headless --offense-agents=2 --defense-agents=1 --agent-play-goalie --no-logging --seed 200 --port 5500 &
 sleep 5
-./../DDPG/src/maddpg_noswitch.py 5000 1 1 201 &
+./../DDPG/src/maddpg_noswitch.py 5500 1 1 201 &
 sleep 5
-./../DDPG/src/maddpg_noswitch.py 5000 1 2 202 &
+./../DDPG/src/maddpg_noswitch.py 5500 1 2 202 &
 sleep 5
-./../DDPG/src/maddpg_noswitch.py 5000 0 3 203 &
+./../DDPG/src/maddpg_noswitch.py 5500 0 3 203 &
 sleep 5
 # python ../DDPG/tflearn/continuous_space_test_agent.py 6000 &
 
