5effa11ac321c71e81fb50af2f1962e75636965e
surajnaircaltech <surajnair.caltech@gmail.com>
2017-09-27 19:54:36 -0700

    old codebase

--- src/maddpg_switch.sh
+++ src/maddpg_switch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-./bin/HFO --frames-per-trial=500 --untouched-time=500 --fullstate --headless --offense-agents=2 --defense-agents=1 --agent-play-goalie --no-logging --seed 4343 --port 4500 &
+./bin/HFO --frames-per-trial=500 --untouched-time=500 --fullstate --no-sync --offense-agents=2 --defense-agents=1 --agent-play-goalie --no-logging --seed 4343 --port 4500 &
 sleep 5
 ./../DDPG/src/maddpg_switch.py 4500 1 1 4343 &
 sleep 5
 ./../DDPG/src/maddpg_switch.py 4500 1 2 4343 &
 sleep 5
 ./../DDPG/src/maddpg_switch.py 4500 0 3 4343 &
-leep 5
+sleep 5
 # python ../DDPG/tflearn/continuous_space_test_agent.py 6000 &
 
 # The magic line
