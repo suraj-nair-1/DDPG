16781896290480b3ed1d53f5e3695958ec113945
surajnaircaltech <surajnair.caltech@gmail.com>
2017-09-27 19:54:36 -0700

    old codebase

--- src/maddpg_switch.py
+++ src/maddpg_switch.py
@@ -20,8 +20,8 @@ from actor_hfo import ActorNetwork
 from critic_hfo_maddpg import CriticNetwork
 
 
-# LOGPATH = "../DDPG/"
-LOGPATH = "/cs/ml/ddpgHFO/DDPG/"
+LOGPATH = "../DDPG/"
+# LOGPATH = "/cs/ml/ddpgHFO/DDPG/"
 
 PRIORITIZED = True
 
