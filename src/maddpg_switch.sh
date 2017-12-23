7b845a7f92c1ca1fd6782f3e93e07e96608dc018
Suraj Nair <snair@ml-login3.cms.caltech.edu>
2017-12-23 01:52:42 -0800

    old codebase merging

--- src/maddpg_switch.sh
+++ src/maddpg_switch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-./bin/HFO --frames-per-trial=500 --untouched-time=500 --fullstate --headless --offense-agents=2 --defense-agents=1 --agent-play-goalie --no-logging --seed 4343 --port 4500 &
+./bin/HFO --frames-per-trial=500 --untouched-time=500 --fullstate --headless --offense-agents=2 --defense-agents=1 --agent-play-goalie --no-logging --seed 5111 --port 5500 &
 sleep 5
-./../DDPG/src/maddpg_switch.py 4500 1 1 4343 &
+./../DDPG/src/maddpg_switch.py 5500 1 1 5111 &
 sleep 5
-./../DDPG/src/maddpg_switch.py 4500 1 2 4343 &
+./../DDPG/src/maddpg_switch.py 5500 1 2 5111 &
 sleep 5
-./../DDPG/src/maddpg_switch.py 4500 0 3 4343 &
+./../DDPG/src/maddpg_switch.py 5500 0 3 5111 &
 leep 5
 # python ../DDPG/tflearn/continuous_space_test_agent.py 6000 &
 
